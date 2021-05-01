bc7301f02afcac60213aed52313d9c3f73ba1295
rockleon <ameyphantom07@gmail.com>
2021-05-01 17:55:49 +0530

    db3

--- config/settings/base.py
+++ config/settings/base.py
@@ -124,10 +124,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Hobby_dev',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'postgres',
+        # 'HOST': 'localhost',
+        # 'PORT': 5432,
     }
 }
 
