5c309e8817c047d4a7c104608ee31bf86789c371
rockleon <ameyphantom07@gmail.com>
2021-05-01 17:07:58 +0530

    run migration

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ import django_heroku
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
