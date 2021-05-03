db1589f795443838b021c11c367e6448a1a82f00
rockleon <ameyphantom07@gmail.com>
2021-05-03 20:26:10 +0530

    admin site add fields to display

--- sitevisits/account/admin.py
+++ sitevisits/account/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Account
 
-admin.site.register(Account)
+class AdminAccount(admin.ModelAdmin):
+    model = Account
+    list_display = ('title', 'key', 'url')
+
+
+admin.site.register(Account, AdminAccount)
