6f877af8016474b3c195863ff08690adc3bbcf69
rockleon <ameyphantom07@gmail.com>
2021-05-03 19:13:56 +0530

    sdf

--- config/settings/base.py
+++ config/settings/base.py
@@ -120,6 +120,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'site_visits',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
