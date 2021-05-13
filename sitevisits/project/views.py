8a341f9c01123fff317603322b60dab55eb9dd89
rockleon <ameyphantom07@gmail.com>
2021-05-13 11:43:01 +0530

    pass author when creating project

--- sitevisits/project/views.py
+++ sitevisits/project/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Project
 
+
 class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = [IsAuthenticated]
@@ -15,17 +16,21 @@ class ProjectViewSet(ModelViewSet):
         else:
             return Project.objects.filter(author=self.request.user)
 
+    def create(self, request, *args, **kwargs):
+        request.data['author'] = str(request.user.id)
+        return super(ProjectViewSet, self).create(request, *args, **kwargs)
+
 
 @api_view(['GET', 'POST'])
 def IncrementVisits(request, key):
     try:
-        acc = Project.objects.get(key = key)
+        acc = Project.objects.get(key=key)
     except:
         return Response({"error": "Invalid key!"}, status=400)
     if request.method == 'GET':
-        return Response({ "title": acc.title }, status=200)
+        return Response({"title": acc.title}, status=200)
     if request.method == 'POST':
         acc.visits += 1
         acc.save()
-        return Response({ "title": acc.title }, status=200)
+        return Response({"title": acc.title}, status=200)
     return Response({"error": "Something went wrong!"}, status=400)
